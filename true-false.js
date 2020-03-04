882720ae4e4b2d61acafb8603f5ca15c9272374f
mdraihanmahmud <digitalmarketer1212@gmail.com>
2020-03-04 08:06:27 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,9 @@
+//true value= -1,1,2.., string- "Raihan", white space " "
+//false value= 0, Single/double quote-''/"", null,NaN
+const age="raihan";
+if(age){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false")
+}
